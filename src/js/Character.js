7ce9a740b8031546c744207d54221ae5fad51895
Regina28-m <regina-railan2013@yandex.ru>
2023-01-27 11:14:49 +0300

    commit

---
+++ src/js/Character.js
@@ -0,0 +1,17 @@
+export default class Character {
+  constructor(name, type) {
+    const list = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+    if (name.length < 2 || name.length > 10 || name === undefined) {
+      throw new Error('Длина имени не соответствует условиям');
+    }
+    if (!list.includes(type)) {
+      throw new Error('Не соответствие заданным типам');
+    }
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
+    this.attack = 0;
+    this.defence = 0;
+  }
+}
