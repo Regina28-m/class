ff41bea7a69d8cfdd46657d6c7a0268547e6ffd6
Regina28-m <81991106+Regina28-m@users.noreply.github.com>
2023-01-28 13:09:50 +0300

    Update and rename src/js/Character.js to Src/js/Character.js

--- Src/js/Character.js
+++ Src/js/Character.js
@@ -11,7 +11,7 @@ export default class Character {
     this.type = type;
     this.health = 100;
     this.level = 1;
-    this.attack = 0;
-    this.defence = 0;
+    this.attack = undefined;
+    this.defence = undefined;
   }
 }
